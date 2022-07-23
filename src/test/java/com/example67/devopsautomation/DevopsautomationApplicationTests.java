9410ad1edd618438c2b0410fdf8e547fcb93db32
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2022-07-23 14:24:55 +0530

    qwerty commit devops automation

---
+++ src/test/java/com/example67/devopsautomation/DevopsautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example67.devopsautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
